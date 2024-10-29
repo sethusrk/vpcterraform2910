45196fc5f7449964b7a75a0dfbe9e98fa23b40e4
sethusrk <sethusrk285@gmail.com>
2024-10-29 15:11:27 +0530

    Add extra lines of code to existing file

--- main.tf
+++ main.tf
@@ -6,3 +6,43 @@ resource "aws_vpc" "myvpc" {
     Name = "myvpc"
   }
 }
+
+resource "aws_subnet" "pubsub1" {
+  vpc_id     = var.vpc_id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
+  
+  tags = {
+    Name = "pubsub1"
+  }
+}
+
+resource "aws_subnet" "pubsub2" {
+  vpc_id     = var.vpc_id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "pubsub2"
+  }
+}
+
+resource "aws_subnet" "prisub" {
+  vpc_id     = var.vpc_id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "prisub"
+  }
+}
+
+resource "aws_internet_gateway" "tigw" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "tigw"
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,4 @@
+variable "vpc_id" {
+  type = string
+  default = "vpc-0dfa5e555b136d7c3"
+}
